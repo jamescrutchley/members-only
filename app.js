4f5c8bdc3092d4ded52d94325a45e59b1c88c382
James Crutchley <jamescrutchley98@gmail.com>
2023-11-24 19:50:52 +1100

    Split code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const path = require("path");
@@ -6,24 +6,19 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const User = require("./models/User");
 
-const mongoDb = process.env.MONGODB_URI || process.env.DEV_URI
+const router = require("./routes/routes");
+
+const mongoDb = process.env.MONGODB_URI || process.env.DEV_URI;
 
 mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
-const User = mongoose.model(
-  "User",
-  new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-  })
-);
-
 const app = express();
-app.set("views", __dirname);
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
@@ -32,12 +27,12 @@ passport.use(
     try {
       const user = await User.findOne({ username: username });
       if (!user) {
-        return done(null, false, { message: "Incorrect username" });
+        return done(null, false, { message: `We couldn't find user '${username}' in our database.` });
       }
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
         // passwords do not match!
-        return done(null, false, { message: "Incorrect password" });
+        return done(null, false, { message: `Incorrect password, ${username}'s password is ${user.password}` });
       }
 
       return done(null, user);
@@ -69,40 +64,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => res.render("index", { user: req.user }));
-
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
-
-app.post("/sign-up", async (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-    try {
-      const user = new User({
-        username: req.body.username,
-        password: hashedPassword,
-      });
-      const result = await user.save();
-      res.redirect("/");
-    } catch (err) {
-      return next(err);
-    }
-  });
-});
-
-app.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/",
-  })
-);
-
-app.get("/log-out", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
+app.use("/", router);
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
